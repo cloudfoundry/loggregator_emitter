ee4d74a2084d6513d8dc19a49c2da379b8fad80c
Stephan Hagemann <stephan@pivotallabs.com>
2013-10-08 16:59:06 -0600

    Fix specs by changing fake server port per test

--- spec/loggregator_emitter/emit_spec.rb
+++ spec/loggregator_emitter/emit_spec.rb
@@ -1,55 +1,63 @@
-require 'support/fake_loggregator_server'
-require 'loggregator_emitter'
+require "support/fake_loggregator_server"
+require "loggregator_emitter"
 
 describe LoggregatorEmitter do
-  describe 'configuring emitter' do
+  class FreePort
+    def self.next_free_port
+      @@next_free_port ||= 12345
+      @@next_free_port += 1
+    end
+  end
+  let(:free_port) do
+    FreePort.next_free_port
+  end
+
+  describe "configuring emitter" do
     describe "valid configurations" do
-      it 'is valid with IP and proper source type' do
-        expect { LoggregatorEmitter::Emitter.new('0.0.0.0:12345', LogMessage::SourceType::DEA) }.not_to raise_error
+      it "is valid with IP and proper source type" do
+        expect { LoggregatorEmitter::Emitter.new("0.0.0.0:12345", LogMessage::SourceType::DEA) }.not_to raise_error
       end
 
-      it 'is valid with resolveable hostname and proper source type' do
-        expect { LoggregatorEmitter::Emitter.new('localhost:12345', LogMessage::SourceType::DEA) }.not_to raise_error
+      it "is valid with resolveable hostname and proper source type" do
+        expect { LoggregatorEmitter::Emitter.new("localhost:12345", LogMessage::SourceType::DEA) }.not_to raise_error
       end
 
-      it 'is valid if a server name is given' do
-        expect { LoggregatorEmitter::Emitter.new('some-unknown-address:12345', LogMessage::SourceType::DEA) }.not_to raise_error
+      it "is valid if a server name is given" do
+        expect { LoggregatorEmitter::Emitter.new("some-unknown-address:12345", LogMessage::SourceType::DEA) }.not_to raise_error
       end
     end
 
     describe "invalid configurations" do
       describe "error based on loggregator_server" do
-        it 'raises if host has protocol' do
-          expect { LoggregatorEmitter::Emitter.new('http://0.0.0.0:12345', LogMessage::SourceType::DEA) }.to raise_error(ArgumentError)
+        it "raises if host has protocol" do
+          expect { LoggregatorEmitter::Emitter.new("http://0.0.0.0:12345", LogMessage::SourceType::DEA) }.to raise_error(ArgumentError)
         end
       end
 
       describe "error based on source_type" do
-        it 'raises if source_type is invalid' do
-          expect { LoggregatorEmitter::Emitter.new('0.0.0.0:12345', 40) }.to raise_error(ArgumentError)
+        it "raises if source_type is invalid" do
+          expect { LoggregatorEmitter::Emitter.new("0.0.0.0:12345", 40) }.to raise_error(ArgumentError)
         end
       end
     end
   end
 
-  # describe "#emit"
-  # describe "#emit_error"
-  {"emit" => LogMessage::MessageType::OUT, "emit_error" => LogMessage::MessageType::ERR}.each do |method, message_type|
-    describe "##{method}" do
+  {"emit" => LogMessage::MessageType::OUT, "emit_error" => LogMessage::MessageType::ERR}.each do |emit_method, message_type|
+    describe "#emit" do
       def make_emitter(host)
-        LoggregatorEmitter::Emitter.new("#{host}:12345", LogMessage::SourceType::CLOUD_CONTROLLER, 42)
+        LoggregatorEmitter::Emitter.new("#{host}:#{free_port}", LogMessage::SourceType::CLOUD_CONTROLLER, 42)
       end
 
       before do
-        @server = FakeLoggregatorServer.new(12345)
+        @server = FakeLoggregatorServer.new(free_port)
         @server.start
       end
 
-      it 'successfully writes protobuffers to a socket' do
+      it "successfully writes protobuffers to a socket" do
         emitter = make_emitter("0.0.0.0")
-        emitter.send(method, "my_app_id", 'Hello there!')
-        emitter.send(method, "my_app_id", 'Hello again!')
-        emitter.send(method, nil, 'Hello again!')
+        emitter.send(emit_method, "my_app_id", "Hello there!")
+        emitter.send(emit_method, "my_app_id", "Hello again!")
+        emitter.send(emit_method, nil, "Hello again!")
 
         @server.wait_for_messages_and_stop(2)
 
@@ -57,35 +65,35 @@ describe LoggregatorEmitter do
 
         expect(messages.length).to eq 2
         message = messages[0]
-        expect(message.message).to eq 'Hello there!'
+        expect(message.message).to eq "Hello there!"
         expect(message.app_id).to eq "my_app_id"
         expect(message.source_type).to eq LogMessage::SourceType::CLOUD_CONTROLLER
         expect(message.source_id).to eq "42"
         expect(message.message_type).to eq message_type
 
         message = messages[1]
-        expect(message.message).to eq 'Hello again!'
+        expect(message.message).to eq "Hello again!"
       end
 
       it "successfully writes protobuffers using a dns name for the loggregator server" do
         emitter = make_emitter("localhost")
-        emitter.send(method, "my_app_id", 'Hello there!')
+        emitter.send(emit_method, "my_app_id", "Hello there!")
 
         @server.wait_for_messages_and_stop(1)
 
         messages = @server.messages
         expect(messages.length).to eq 1
-        expect(messages[0].message).to eq 'Hello there!'
+        expect(messages[0].message).to eq "Hello there!"
       end
     end
   end
 
   describe "source id" do
     let(:emit_message) do
-      server = FakeLoggregatorServer.new(12345)
+      server = FakeLoggregatorServer.new(free_port)
       server.start
 
-      @emitter.emit_error("my_app_id", 'Hello there!')
+      @emitter.emit_error("my_app_id", "Hello there!")
 
       server.wait_for_messages_and_stop(2)
 
@@ -93,17 +101,17 @@ describe LoggregatorEmitter do
     end
 
     it "can be nil" do
-      @emitter = LoggregatorEmitter::Emitter.new('0.0.0.0:12345', LogMessage::SourceType::CLOUD_CONTROLLER)
+      @emitter = LoggregatorEmitter::Emitter.new("0.0.0.0:#{free_port}", LogMessage::SourceType::CLOUD_CONTROLLER)
       expect(emit_message.source_id).to eq nil
     end
 
     it "can be passed in as a string" do
-      @emitter = LoggregatorEmitter::Emitter.new('0.0.0.0:12345', LogMessage::SourceType::CLOUD_CONTROLLER, "some_source_id")
+      @emitter = LoggregatorEmitter::Emitter.new("0.0.0.0:#{free_port}", LogMessage::SourceType::CLOUD_CONTROLLER, "some_source_id")
       expect(emit_message.source_id).to eq "some_source_id"
     end
 
     it "can be passed in as an integer" do
-      @emitter = LoggregatorEmitter::Emitter.new('0.0.0.0:12345', LogMessage::SourceType::CLOUD_CONTROLLER, 13)
+      @emitter = LoggregatorEmitter::Emitter.new("0.0.0.0:#{free_port}", LogMessage::SourceType::CLOUD_CONTROLLER, 13)
       expect(emit_message.source_id).to eq "13"
     end
   end
