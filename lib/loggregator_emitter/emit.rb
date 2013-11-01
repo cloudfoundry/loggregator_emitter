858555e6a4114a102836f90546b0fd501118ff4c
Casey McTaggart <cmctaggart@pivotallabs.com>
2013-11-01 10:08:55 -0600

    Add performance tests for emitter
    [#59424930]
    
    Signed-off-by: Glenn Oppegard <goppegard@pivotallabs.com>

--- lib/loggregator_emitter/emit.rb
+++ lib/loggregator_emitter/emit.rb
@@ -32,10 +32,14 @@ module LoggregatorEmitter
       @host && !@host.match(/:\/\//)
     end
 
+    def split_message(message)
+      message.split(/\n|\r/).reject { |a| a.empty? }
+    end
+
     def emit_message(app_id, message, type)
       return unless app_id && message && message.strip.length > 0
 
-      message.split(/\r\n|\n\r|\n|\r/).each do |m|
+      split_message(message).each do |m|
         if m.bytesize > MAX_MESSAGE_BYTE_SIZE
           m = m.byteslice(0, MAX_MESSAGE_BYTE_SIZE-TRUNCATED_STRING.bytesize) + TRUNCATED_STRING
         end
@@ -44,7 +48,7 @@ module LoggregatorEmitter
           send_protobuffer(create_log_message(app_id, m, type))
         else
           send_protobuffer(create_log_envelope(app_id, m, type))
-	    end
+	      end
       end
     end
 
---
+++ spec/loggregator_emitter/emit_performance_spec.rb
@@ -0,0 +1,65 @@
+require "support/fake_loggregator_server"
+require "loggregator_emitter"
+
+class MessageFixture
+  attr_reader :name, :message
+
+  def initialize(name, message, server_threshold, no_server_threshold)
+    @name = name
+    @message = message
+    @server_threshold = server_threshold
+    @no_server_threshold = no_server_threshold
+  end
+
+  def expected(using_server)
+    using_server ? @server_threshold : @no_server_threshold
+  end
+end
+
+class FreePort
+  def self.next_free_port
+    @@next_free_port ||= 12345
+    @@next_free_port += 1
+  end
+end
+
+shared_examples "a performance test" do |fixture, using_server|
+  let(:free_port) { FreePort.next_free_port }
+  let(:iterations) { using_server ? 100 : 1000 }
+  subject(:emitter) { LoggregatorEmitter::Emitter.new("localhost:#{free_port}", "API", 42, "my-secret") }
+
+  before do
+    if using_server
+      @server = FakeLoggregatorServer.new(free_port)
+      @server.start
+    else
+      emitter.should_receive(:send_protobuffer).at_least(iterations).times
+    end
+  end
+
+  after do
+    @server.stop if using_server
+  end
+
+  it "emits #{fixture.name} within a time threshold #{using_server ? 'with' : 'without'} server" do
+    start_time = Time.now.to_f
+
+    iterations.times { emitter.emit("my_app_id", fixture.message) }
+
+    expect(Time.now.to_f - start_time).to be < fixture.expected(using_server)
+  end
+
+end
+
+describe LoggregatorEmitter do
+  @fixtures = []
+  @fixtures << MessageFixture.new("long_message", (124*1024).times.collect { "a" }.join(""), 0.3, 0.5)
+  @fixtures << MessageFixture.new("message with newlines", 10.times.collect { (6*1024).times.collect { "a" }.join("") + "\n" }.join(""), 1.0, 1.0)
+  @fixtures << MessageFixture.new("message worst case", (124*1024).times.collect { "a" }.join("") + "\n", 0.5, 1.0)
+
+  [true, false].each do |using_server|
+    @fixtures.each do |fixture|
+      it_behaves_like "a performance test", fixture, using_server
+    end
+  end
+end
